92c04e7026fe723d0fad92e5b3c0e004f8b090c4
John Westhoff <johnjwesthoff@gmail.com>
2019-11-03 12:34:12 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pycaboose',
+    version='1.0.0',
+    author='John Westhoff',
+    author_email='johnjwesthoff@gmail.com',
+    description=('A python library for persisting data within a script.'),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/JohnathonNow/pycaboose',
+    packages=setuptools.find_packages(),
+    test_suite='tests',
+    include_package_data=True,
+)
